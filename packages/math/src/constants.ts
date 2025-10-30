144c5e2ec4dd862cfbd8df1a7cfa36a74862c0df
Ricardo Miranda <rmiranda@teco.com.ar>
2025-10-30 10:28:31 -0300

    Math constants

--- packages/math/src/constants.ts
+++ packages/math/src/constants.ts
@@ -2,12 +2,19 @@ export const EPSILON = 1e-10;
 export const TAU = Math.PI * 2;
 export const PHI = (1 + Math.sqrt(5)) / 2;
 export const SQRT2 = Math.SQRT2;
+export const SQRT1_2 = Math.SQRT1_2;
 export const SQRT3 = Math.sqrt(3);
+export const SQRT3_2 = Math.sqrt(3) * 0.5;
 export const LN2 = Math.LN2;
 export const LN10 = Math.LN10;
 
 export const DEG_TO_RAD = Math.PI / 180;
 export const RAD_TO_DEG = 180 / Math.PI;
+
+export const PI = Math.PI;
+export const TWO_PI = 2 * Math.PI;
+export const HALF_PI = 0.5 * Math.PI;
+export const QUARTER_PI = 0.25 * Math.PI;
 
 export const ANGLE_RIGHT = 0;
 export const ANGLE_UP = Math.PI / 2;
