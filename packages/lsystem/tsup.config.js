c0bb796d0bdb61b34c9ae8e4b6250a01d5a4f091
Ricardo Miranda <rmiranda@teco.com.ar>
2025-10-31 16:45:41 -0300

    Update LSystem tsup config

--- packages/lsystem/tsup.config.js
+++ packages/lsystem/tsup.config.js
@@ -1,10 +1,4 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup';
+import { baseConfig } from '../../tsup.base.config.js';
 
-export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
-  dts: true, // Generate declaration file (.d.ts)
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-});
+export default defineConfig(baseConfig);
