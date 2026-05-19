d1ba3be9b7ea039c604a3099515e243310bc497d
Ricardo Miranda <rmiranda@teco.com.ar>
2026-05-19 17:04:00 -0300

    Sepia tests

--- packages/pixbit/tests/pixels.test.ts
+++ packages/pixbit/tests/pixels.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { add, colorPop, duotone, grayscale, halftone, heatmap, multiply, negative, posterize } from "../src";
+import { add, colorPop, duotone, grayscale, halftone, heatmap, multiply, negative, posterize, sepia } from "../src";
 import { pixel } from "../src/types";
 
 describe(add.name, () => {
@@ -344,6 +344,37 @@ describe(posterize.name, () => {
 
   it.each(testData)("Posterizes an image pixels", ({ data, channels, expected }) => {
     const result = posterize(data, channels);
+    expect(result).toEqual(expected);
+  });
+});
+
+describe(sepia.name, () => {
+  const testData: {
+    data: Uint8ClampedArray<ArrayBuffer>;
+    expected: Uint8ClampedArray<ArrayBuffer>;
+  }[][] = [
+    [
+      {
+        data: new Uint8ClampedArray([255, 255, 255, 255]),
+        expected: new Uint8ClampedArray([255, 255, 239, 255]),
+      }
+    ],
+    [
+      {
+        data: new Uint8ClampedArray([0, 0, 0, 255]),
+        expected: new Uint8ClampedArray([0, 0, 0, 255]),
+      }
+    ],
+    [
+      {
+        data: new Uint8ClampedArray([128, 128, 128, 255]),
+        expected: new Uint8ClampedArray([173, 154, 120, 255]),
+      }
+    ],
+  ];
+
+  it.each(testData)("Applies a sepia effect to the image data", ({ data, expected }) => {
+    const result = sepia(data);
     expect(result).toEqual(expected);
   });
 });
