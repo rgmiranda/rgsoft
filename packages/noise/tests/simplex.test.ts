ac9c5e38097341338a11b8c326a174b7bfd2283e
Ricardo Miranda <rmiranda@teco.com.ar>
2026-06-16 12:29:02 -0300

    Simplex noise2 tests

--- packages/noise/tests/simplex.test.ts
+++ packages/noise/tests/simplex.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from "vitest";
 import { Simplex } from "../src";
 
+const G2 = (3 - Math.sqrt(3)) / 6;
+
 describe(Simplex.name, () => {
   const simplex = new Simplex("test-seed");
 
@@ -44,8 +46,8 @@ describe(Simplex.name, () => {
 
     it("should generate zero values on integers", () => {
       const value1 = simplex.noise2(0, 0);
-      const value2 = simplex.noise2(1, 1);
-      const value3 = simplex.noise2(40, 40);
+      const value2 = simplex.noise2(1 - 2 * G2, 1 - 2 * G2);
+      const value3 = simplex.noise2(4 - 5 * G2, 1 - 5 * G2);
       expect(value1).toBeCloseTo(0);
       expect(value2).toBeCloseTo(0);
       expect(value3).toBeCloseTo(0);
